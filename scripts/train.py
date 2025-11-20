cd5da357530cb2b870224b99135252b419d704e6
Benjamin Lash <blash20@moundsparkacademy.org>
2025-11-20 12:56:26 -0500

    track classifier

--- scripts/train.py
+++ scripts/train.py
@@ -7,12 +7,12 @@ from pathlib import Path
 # TODO: For Part 2 (pipeline) load hyperparameters from params.yaml
 
 # TODO: Fill in hyperparameters
-n_estimators = None
-max_depth = None
-random_state = None
+n_estimators = 5
+max_depth = 10
+random_state = 3
 
 # TODO: Set the data path (different for Part 1 vs Part 2)
-data_path = None
+data_path = "./data/raw/data.csv"
 
 df = pd.read_csv(data_path)
 
